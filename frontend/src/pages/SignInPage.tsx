0a0290855a78123e5de1708527772c59636aed29
Jason Widjaja <z5494973@ad.unsw.edu.au>
2026-08-07 19:01:04 +1000

    initial setup frontend only

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,9 +1,7 @@
 export default function SignInPage() {
   return (
     <main className="mx-auto flex min-h-[calc(100svh-4rem)] w-full max-w-360 items-center justify-center px-6 lg:px-8 xl:px-10">
-      <div className="w-full max-w-md">
         TODO Sign In Page
-      </div>
     </main>
   )
 }
